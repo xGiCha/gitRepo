1b37dc31126e7cea65153ff26e1eb6616d05a2c8
ichatzigiannis <ichatzigiannis@cognity.gr>
2021-08-02 17:15:19 +0300

    Optimaze files

--- app/src/main/java/gr/repo/gitrero/models/commits/Commit.kt
+++ app/src/main/java/gr/repo/gitrero/models/commits/Commit.kt
@@ -6,16 +6,8 @@ import com.google.gson.annotations.SerializedName
 data class Commit(
     @SerializedName("author")
     val author: AuthorX,
-    @SerializedName("comment_count")
-    val commentCount: Int,
-    @SerializedName("committer")
-    val committer: Committer,
     @SerializedName("message")
     val message: String,
-    @SerializedName("tree")
-    val tree: Tree,
     @SerializedName("url")
-    val url: String,
-    @SerializedName("verification")
-    val verification: Verification
+    val url: String
 )
--- app/src/main/java/gr/repo/gitrero/models/commits/CommitsItem.kt
+++ app/src/main/java/gr/repo/gitrero/models/commits/CommitsItem.kt
@@ -4,22 +4,8 @@ package gr.repo.gitrero.models.commits
 import com.google.gson.annotations.SerializedName
 
 data class CommitsItem(
-    @SerializedName("author")
-    val author: Author,
-    @SerializedName("comments_url")
-    val commentsUrl: String,
     @SerializedName("commit")
     val commit: Commit,
-    @SerializedName("committer")
-    val committer: CommitterX,
-    @SerializedName("html_url")
-    val htmlUrl: String,
-    @SerializedName("node_id")
-    val nodeId: String,
-    @SerializedName("parents")
-    val parents: List<Parent>,
     @SerializedName("sha")
-    val sha: String,
-    @SerializedName("url")
-    val url: String
+    val sha: String
 )
